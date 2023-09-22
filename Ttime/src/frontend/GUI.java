76ef168b0af57087a9b424fc71129e796324ada0
jinwoo baek <jinwoo060360@gmail.com>
2023-09-22 23:24:32 +0900

    프레임만 짬

--- Ttime/src/frontend/GUI.java
+++ Ttime/src/frontend/GUI.java
@@ -4,7 +4,29 @@ import java.awt.*;
 
 public class GUI extends JFrame {
 	public GUI() {
+		setTitle("Ttime");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container MainPane = getContentPane();
 		
+		JPanel calendar = new JPanel();
+		calendar.setLayout(new GridLayout(9, 6)); //시간표를 나타낼 패널
+		
+		JPanel cemester = new JPanel(); //학기를 선택할 수 있는 패널
+		
+		JPanel grades = new JPanel(); //성적 데이터를 표기할 패널
+		
+		JPanel classes = new JPanel();
+		classes.setLayout(new BorderLayout()); //각 강의의 검색창을 담을 패널
+		
+		MainPane.setLayout(new BorderLayout());
+		
+		MainPane.add(calendar, BorderLayout.CENTER);
+		MainPane.add(cemester, BorderLayout.NORTH);
+		MainPane.add(grades, BorderLayout.SOUTH);
+		MainPane.add(classes, BorderLayout.WEST);
+		
+		setSize(300, 200);
+		setVisible(true);
 	}
 
 }
