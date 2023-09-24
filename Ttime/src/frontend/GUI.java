c937af52d224d9c26009fcae8371e8dc4ec0d2bd
jinwoo baek <jinwoo060360@gmail.com>
2023-09-24 15:18:34 +0900

    기능 간략히 구현

--- Ttime/src/frontend/GUI.java
+++ Ttime/src/frontend/GUI.java
@@ -11,11 +11,16 @@ public class GUI extends JFrame {
 		JPanel calendar = new JPanel();
 		calendar.setLayout(new GridLayout(9, 6)); //시간표를 나타낼 패널
 		
+		String[] cemester_list = {"1-1", "1-2", "2-1", "2-2", "3-1", "3-2", "4-1", "4-2"};
+		
 		JPanel cemester = new JPanel(); //학기를 선택할 수 있는 패널
 		cemester.setLayout(new FlowLayout());
+		
 		JPanel cemester_inner = new JPanel(); //패널 안 버튼을 놓을 패널
 		cemester_inner.setLayout(new FlowLayout());
-		cemester_inner.add(new JButton("학기")); //나중에 선택창으로 수정
+		
+		JComboBox<String> cemester_combo = new JComboBox<String>(cemester_list); //학기 선택창을 구현할 콤보버튼
+		cemester_inner.add(cemester_combo);
 		cemester_inner.add(new JButton("수정"));
 		cemester.add(cemester_inner);
 		
@@ -35,6 +40,10 @@ public class GUI extends JFrame {
 		search.add(new JTextField(10));
 		classes.add(search, BorderLayout.NORTH);
 		
+		String[] test_list = {"테스트1", "테스트2", "테스트3", "테스트4", "테스트5"};
+		JList<String> class_list = new JList<String>(test_list);
+		
+		classes.add(new JScrollPane(class_list), BorderLayout.CENTER);
 		
 		MainPane.setLayout(new BorderLayout());
 		
