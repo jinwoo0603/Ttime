b3c34c8640d59577797c3091849355591f6d17f9
jinwoo baek <jinwoo060360@gmail.com>
2023-09-24 15:26:05 +0900

    주석 또 추가

--- Ttime/src/frontend/GUI.java
+++ Ttime/src/frontend/GUI.java
@@ -40,7 +40,7 @@ public class GUI extends JFrame {
 		search.add(new JTextField(10));
 		classes.add(search, BorderLayout.NORTH);
 		
-		String[] test_list = {"테스트1", "테스트2", "테스트3", "테스트4", "테스트5"};
+		String[] test_list = {"테스트1", "테스트2", "테스트3", "테스트4", "테스트5"}; //DB에서 받아온 데이터
 		JList<String> class_list = new JList<String>(test_list); //현재 과목 리스트
 		
 		classes.add(new JScrollPane(class_list), BorderLayout.CENTER);
