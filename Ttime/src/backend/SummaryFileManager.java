6b75da179c1a709898d2dee241cba28a2aca3827
seojintv <119040029+seojintv@users.noreply.github.com>
2023-10-23 19:57:31 +0900

    Update SummaryFileManager.java

--- Ttime/src/backend/SummaryFileManager.java
+++ Ttime/src/backend/SummaryFileManager.java
@@ -2,12 +2,14 @@
 
 import java.io.*;
 import java.util.List;
+import java.util.ArrayList;
 
 public class SummaryFileManager {
     // 총괄 파일에 총괄 정보를 저장하는 메소드
     public static void saveSummaryToFile(String raw_data, List<String []> summaryData) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(raw_data))) {
             for (String summaryLine : summaryData) {
+                String line = String.join(",", summaryLine);
                 writer.println(summaryLine); // 한 줄씩 총괄 정보를 파일에 저장
             }
         } catch (IOException e) {
@@ -21,7 +23,8 @@ public class SummaryFileManager {
         try (BufferedReader reader = new BufferedReader(new FileReader(raw_data))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                summaryData.add(line); // 파일에서 한 줄씩 읽어와 총괄 정보를 List에 추가
+                String[] summaryLine = line.split(","); // 파일에서 한 줄을 쉼표로 분할하여 배열에 저장
+                summaryData.add(summaryLine); // 총괄 정보를 List에 추가
             }
         } catch (IOException e) {
             e.printStackTrace(); // 파일 읽기 중 발생하는 예외 출력
