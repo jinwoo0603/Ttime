5f432f14e97d9463bfddc9f50403e2aaa82974bc
seojintv <119040029+seojintv@users.noreply.github.com>
2023-11-10 17:18:33 +0900

    Update TimetableFileManager.java

--- Ttime/src/backend/TimetableFileManager.java
+++ Ttime/src/backend/TimetableFileManager.java
@@ -15,16 +15,16 @@ try (PrintWriter writer = new PrintWriter(new FileWriter(raw_data))) {
         }
 
     //파일에서 시간표 데이터를 읽어오는 메소드
-    public static TimetableData readTimetableFromFile(String[] raw_data) {
-        TimetableData timetableData = new TimetableData();
+    public static data readTimetableFromFile(String[] raw_data) {
+        data timetableData = new data();
         try (BufferedReader reader = new BufferedReader(new FileReader(raw_data))) {
             String[] line;
             while ((line = reader.readLine()) != null) {
-                timetableData.addCourse(line); // 파일에서 한 줄씩 읽어와 강의 데이터 추가
+                data.addCourse(line); // 파일에서 한 줄씩 읽어와 강의 데이터 추가
             }
         } catch (IOException e) {
             e.printStackTrace(); // 파일 읽기 중 발생하는 예외 처리
         }
-        return timetableData;
+        return data;
     }
 }
